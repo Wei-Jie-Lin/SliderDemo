a86c872e0c340d0c0894d9cc16567ee87978d168
JayLin <leoj72955@gmail.com>
2020-07-19 10:01:18 +0800

    Initial Commit

---
+++ SliderDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderDemo
+//
+//  Created by Jay Lin on 2020/7/19.
+//  Copyright © 2020 Jay Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
